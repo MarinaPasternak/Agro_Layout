0dde92ff6bd1f1f9896eb9216e8327f9b9477af0
Marina Pasternak <256pasternakmarina@gmail.com>
2021-03-26 12:48:08 +0300

    refactoring and add vue.js

---
+++ Vue.js/tomato/js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        products:[
+            {id:1, title:"Prod 1", short_text:'short_text', image:'img/1.jpg', desc:"Full desc"},
+            {id:1, title:"Prod 2", short_text:'short_text', image:'img/2.jpg', desc:"Full desc"},
+            {id:1, title:"Prod 3", short_text:'short_text', image:'img/3.jpg', desc:"Full desc"},
+            {id:1, title:"Prod 4", short_text:'short_text', image:'img/4.jpg', desc:"Full desc"},
+            {id:1, title:"Prod 5", short_text:'short_text', image:'img/5.jpg', desc:"Full desc"}
+    ]
+    },
+  });
